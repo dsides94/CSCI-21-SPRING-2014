db1edfdb25d2830cbeb4ede3f8af19c7ca3a1fc1
OpenShift guest <dsides94@yahoo.com>
2014-04-02 13:18:18 -0400

    done

--- Project2/box.cpp
+++ Project2/box.cpp
@@ -96,16 +96,3 @@ Prize Box::removePrize(unsigned int index)
     }
     
 }
-
-/*
-Box (check UNIT TEST for required API)
-private data members: boxNumber (unsigned int), boxColor (string), scratch (Prize), prizes (Prize*, for dynamic array), prizeCapacity (unsigned int), prizeCount (unsigned int)
-public default constructor: initial values boxNumber (0), boxColor ("NO COLOR"), prizeCapacity (5), prizeCount(0), prizes (array initialized to match prizeCapacity)
-public overloaded constructor: parameters for boxNumber, boxColor, prizeCapacity, prizes (array initialized to match prizeCapacity)
-public destructor: free memory associated with prizes
-public accessors/gets AND mutators/sets for boxNumber, boxColor
-public accessor/get ONLY for data members prizeCapacity, prizeCount
-public addPrize: parameters prize (Prize), return value (bool); place prize in prizes array if there is space and return true, else return false
-public getPrize: parameters index (unsigned int), return value Prize&; return a Prize if index is valid, else return scratch (data member declared in class header)
-public removePrize: parameters index (unsigned int), return value Prize; remove and return Prize if index is valid, else return scratch (data member declared in class header)
-*/
