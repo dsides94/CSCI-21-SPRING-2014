fadabdcb18abe20180ab91937b8c413a37cb35b1
OpenShift guest <dsides94@yahoo.com>
2014-04-09 12:57:51 -0400

    added comments, make pc17 and pc18 seperate

--- SList/SList.h
+++ SList/SList.h
@@ -5,16 +5,45 @@
 
 class SList{
     public:
+        /*
+         * Default constructor.
+         * Initializes head to NULL and size to 0.
+         */
         SList();
+        
+        /*
+         * Default deconstructor that deletes all nodes in this list, by
+         * calling the clear function.
+         */
         ~SList();
         
+        /*
+         * Adds a node to this list, and set it to the head of the list.
+         * @param contents an int that is the contents of the new node.
+         */
         void insertHead(int contents);
+        
+        /*
+         * Removes the head node for this list.
+         */
         void removeHead();
         
+        /*
+         * Deletes every node in this list, clearing it.
+         */
         void clear();
         
+        /*
+         * Get the size of this list, which is the number of nodes in it.
+         * @return an unsigned int that is the size of this list.
+         */
         unsigned int getSize() const;
         
+        /*
+         * Builds a string representation of the contents of every node in
+         * this list.
+         * @return a string that is the string representation of this list.
+         */
         std::string toString() const;
         
     private:
