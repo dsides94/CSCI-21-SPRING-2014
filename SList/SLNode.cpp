37a2e4ea2f26a4dcfe710113fdce53043d390b01
OpenShift guest <dsides94@yahoo.com>
2014-04-09 12:31:56 -0400

    d

--- SList/SLNode.cpp
+++ SList/SLNode.cpp
@@ -38,27 +38,3 @@ SLNode* SLNode::getNextNode() const
 {
     return nextNode;
 }
-
-/*Declare and implement a class that provides the following interface EXACTLY.
-
-class name: SLNode
-
-private data members:
-
-	nextNode, SLNode* that points to the next node in a singly-linked list
-	contents, int used to store the data contents of this SLNode
-
-public function interface:
-
-	default constructor, sets values nextNode(NULL) and contents(0)
-	overloaded constructor, int parameter assigned to contents
-	
-	destructor, sets nextNode to NULL
-	
-	void setContents (int)
-	int getContents () const
-	
-	void setNextNode (SLNode*)
-	
-	SLNode* getNextNode () const
-*/
